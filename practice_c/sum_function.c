8c0a6a1db8abba93aa24dc7e82212e41e7b45f41
Harshit Kumar Sinha <neetusinha000@gmail.com>
2026-03-06 23:04:38 +0530

    sum function

---
+++ practice_c/sum_function.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int add(int a,int b){
+    return a+b;
+}
+
+int main(){
+    int a, b, result;
+    printf("enter first number:");
+    scanf("%d", &a);
+    printf ("enter second number:");
+    scanf ("%d", &b);
+    result = add(a,b);
+    printf("sum=%d \n", result);
+    return 0;
+}
